49cd636459989187b873911a08071f93a0b9ac6c
Isaac <isaacjf321@gmail.com>
2026-05-14 13:54:11 -0300

    melhoramentos do jogo

--- ImpostorIA/Program.cs
+++ ImpostorIA/Program.cs
@@ -1,6 +1,8 @@
-﻿using Google.GenAI; //para usar os comandos do GoogleAi 
-using DotNetEnv; //para usar os comandos do env 
+﻿using DotNetEnv; //para usar os comandos do env 
+using Google.GenAI; //para usar os comandos do GoogleAi 
+using Google.GenAI.Types;
 using System;
+using System.Threading; //para usar sleep 
  //dependencia para poder utilizar as funções para API do Gemini 
 
 namespace ImpostorIA
@@ -11,13 +13,14 @@ namespace ImpostorIA
         
         
         
-        private static Client client;   //declarou o cliente 
+        private static Client client=null!;   //declarou o cliente 
         public static async Task Main(string[] args)       //é necessario esse tipo de função para utilizar o await
         {
             var root = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName; //forçando para emtrar na pasta do env  
             
             Env.Load(Path.Combine(root, ".env")); //carregando o env dentro da aplicação
-            var apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY"); //pegando a chave da api dentro do .env 
+            var apiKey = System.Environment.GetEnvironmentVariable("GOOGLE_API_KEY")
+                        ?? throw new Exception("GOOGLE_API_KEY não encontrada no .env!"); //pegando a chave da api dentro do .env 
             client = new Client(apiKey:  apiKey); //instanciando o client 
             
             //uma forma de colocar a apikey
@@ -188,6 +191,7 @@ namespace ImpostorIA
 
         static async Task Rodada(List<string> jogadores)
         {
+            List<string> palavrasJa = new List<string>();
             Console.Clear();
             if (jogadores.Count < 3)
             {
@@ -198,9 +202,11 @@ namespace ImpostorIA
             Console.WriteLine("IMPOSTOR IA - INICIO DE RODADA");
             Console.WriteLine("Qual será o TEMA dessa jogada?");
             string tema = Console.ReadLine();
-            var palavraSorteada = await EscolhendoTema(tema); //var é a variavel que se adapta 
+            var palavraSorteada = await EscolhendoTema(tema, palavrasJa); //var é a variavel que se adapta 
             //tudo que envolve a IA e sua resposta, tem q usar o await para esperar a resposta dele 
-            //await é uma corrente se um metodo la dentro usa , o método mais de fora tbm vai usar 
+            //await é uma corrente se um metodo la dentro usa , o método mais de fora tbm vai usar
+
+
             string impostor = SorteioImpostor(jogadores);
             Console.WriteLine("Palavra e impostor foram sorteados!");
             Pausar();
@@ -229,17 +235,41 @@ namespace ImpostorIA
             Pausar();
         }
 
-        static async Task<string> EscolhendoTema(string tema) //oq é esperado retornar dessa função
+        static async Task<string> EscolhendoTema(string tema, List<string> palavraJa) //oq é esperado retornar dessa função
         {
-            var respostaIA = await client.Models.GenerateContentAsync(
+            bool achado = false;
+            GenerateContentResponse? respostaIA = null; //variavel para receber a resposta da IA, o ? é para dizer que ela pode ser nula
+
+            Console.Write("Carregando");
+            for(int i = 0;i<3; i++)
+            {
+                Console.Write(".");
+                await Task.Delay(1000); //animação do carregamento
+            }
+
+            do
+            {
+                respostaIA = await client.Models.GenerateContentAsync(
                 model: "gemini-2.5-flash-lite", //modelo da IA
-                contents: $"Você é um gerador de palavras de um jogo. O tema é {tema}. Responda com apenas uma palavra ou nome famoso/conhecido relacionado ao tema, sem pontuação e sem explicação");
-                //prompt realizado nele 
-            return respostaIA.Text;
+                contents: $"Você é um gerador de palavras de um jogo. O tema é {tema}. Responda com apenas uma palavra ou nome famoso/conhecido relacionado ao tema, sem pontuação e sem explicação, mas não seja tão obvio");
+
+                string busca = palavraJa.Find(x=> x == respostaIA.Text); //verificando se a palavra já foi usada
+
+                
+
+                if( busca == null)
+                {
+                    achado = true;
+                    palavraJa.Add(respostaIA.Text); //adicionando palavra já utilizada na lista            
+                }     
+
+                //acessando o modelo(Models) que a API vai comuninar e tbm o comando que vai ser realizado nele
+                //await é pra esperar a resposta do gemini 
+                //GenerateContentAsync é a geração da resposta da IA 
+            }while(achado == false);
             
-            //acessando o modelo(Models) que a API vai comuninar e tbm o comando que vai ser realizado nele
-            //await é pra esperar a resposta do gemini 
-            //GenerateContentAsync é a geração da resposta da IA 
+            Console.WriteLine("OK");
+            return respostaIA.Text!;
         }
 
         static string SorteioImpostor(List<string> jogadores)
